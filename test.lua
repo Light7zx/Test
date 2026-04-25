53dee4996a102636b08ed3019853036489c1774b
Light7zx <azurekite1128@gmail.com>
2026-04-25 16:57:29 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,7 @@
-print("Script jalan")
+print("Script berhasil jalan!")
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Test Script",
+    Text = "Berhasil dieksekusi!",
+    Duration = 5
+})
